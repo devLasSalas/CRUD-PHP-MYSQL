12bb2207f2fe74f74f06b159580a43b9e5d4b6d0
Bene <benedetty321@gmail.com>
2023-02-06 11:52:25 -0500

    first commit

---
+++ inc/navbar.php
@@ -0,0 +1,40 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">Usuarios</a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">Nuevo</a>
+              <a class="navbar-item">Lista</a>
+              <a class="navbar-item">Buscar</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary is-rounded">
+            Mi cuenta
+          </a>
+          <a class="button is-rounded">
+            Salir
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+  include './inc/head.php'
+  ?>
+</head>
+
+<body>
+  <?php
+  include './inc/navbar.php';
+  ?>
+</body>
+
+</html>
