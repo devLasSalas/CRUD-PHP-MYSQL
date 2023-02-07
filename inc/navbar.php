606afbd80df8897c69dd45c45797b09493c9feb0
Bene <benedetty321@gmail.com>
2023-02-07 10:37:40 -0500

    ultimo commit

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      <img src="./img/logo.png" width="68" height="38">
     </a>
 
     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
@@ -13,15 +13,37 @@
 
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
+
         <div class="navbar-item has-dropdown is-hoverable">
           <a class="navbar-link">Usuarios</a>
-
             <div class="navbar-dropdown">
               <a class="navbar-item">Nuevo</a>
+              <a class="navbar-item">Lista</a>
+              <a class="navbar-item">Buscar</a> 
+            </div>
+        </div>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">Categorias</a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">Nueva</a>
               <a class="navbar-item">Lista</a>
               <a class="navbar-item">Buscar</a>
             </div>
         </div>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">Productos</a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">Nuevo</a>
+              <a class="navbar-item">Lista</a>
+              <a class="navbar-item">Por categorias</a>
+              <a class="navbar-item">Buscar</a>
+            </div>
+        </div>
+    </div>
     </div>
 
     <div class="navbar-end">
--- index.php
+++ index.php
@@ -1,15 +1,16 @@
+<?php require './inc/session_start.php'?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <?php
-  include './inc/head.php'
-  ?>
+  <?php include './inc/head.php'?>
 </head>
 
 <body>
   <?php
   include './inc/navbar.php';
+  include './inc/script.php';
   ?>
 </body>
 
